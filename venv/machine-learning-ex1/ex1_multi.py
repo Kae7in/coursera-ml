c1b02939c2299586f3b221e323a2fdf51cfa650d
Kaelin Hooper <ka3lin@gmail.com>
2018-03-23 23:35:22 -0500

    Cleaned up and commented ex1_multi.py

--- venv/machine-learning-ex1/ex1_multi.py
+++ venv/machine-learning-ex1/ex1_multi.py
@@ -12,7 +12,7 @@ from featureNormalize import feature_normalize
 from normalEqn import normal_eqn
 
 if __name__ == '__main__':
-    # ================ Part 1: Feature Normalization ================
+    ''' ================ Part 1: Feature Normalization ================ '''
 
     # Read the data file
     data = np.loadtxt('ex1data2.txt', delimiter=',')
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     plt.ylabel('Price in $')
     plt.xlabel('sq.ft.')
     plt.axis([0, 5000, 0, 800000])
-    # plt.show()
+    plt.show()
 
     # Plot beds against price
     plt.figure()
@@ -35,30 +35,45 @@ if __name__ == '__main__':
     plt.ylabel('Price in $')
     plt.xlabel('# of beds')
     plt.axis([0, 6, 0, 800000])
-    # plt.show()
+    plt.show()
 
+    # Feature Normalize
     X_norm, mu, sigma = feature_normalize(X)
+
+    # Append x-sub-0 (vector of 1's)
     X_norm = np.concatenate((np.ones((m, 1)), X_norm), axis=1)
 
+    ''' ================ Part 2: Gradient Descent ================ '''
+
+    # Set gradient descent parameters
     alpha = 0.01
     iterations = 1000
     thetas1 = np.zeros((3, 1))
 
+    # Gradient descent
     thetas1, j_history = gradient_descent(X_norm, y, thetas1, alpha, iterations)
 
+    # Prediction test
     test1 = np.array([1650, 3]).reshape(1, 2)
     test1 = (test1 - mu) / sigma  # normalize input values
     test1 = np.concatenate((np.ones((1, 1)), test1), axis=1)
     predict1 = np.dot(test1, thetas1)
-    # print(thetas)
+    # print(thetas1)
     print('Predicted price of a 1650 sq-ft, 3 br house: \n', predict1[0][0])
 
-    #  Using the Normal Equation
-    X_copy = X.copy()
-    X_copy = np.concatenate((np.ones((m, 1)), X_copy), axis=1)
-    thetas2 = normal_eqn(X_copy, y)
+    ''' ================ Part 3: Normal Equations ================ '''
 
-    # print(thetas)
+    # Let's try another way...
+    X_copy = X.copy()
+
+    # Append x-sub-0 (vector of 1's)
+    X_copy = np.concatenate((np.ones((m, 1)), X_copy), axis=1)
+
+    # Use the Normal Equation
+    thetas2 = normal_eqn(X_copy, y)
+    # print(thetas2)
+
+    # Prediction test
     test2 = np.array([1, 1650, 3]).reshape(1, 3)
     predict2 = np.dot(test2, thetas2)
     print('Predicted price of a 1650 sq-ft, 3 br house: \n', predict2[0][0])
